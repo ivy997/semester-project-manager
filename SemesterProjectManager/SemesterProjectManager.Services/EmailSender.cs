c28d067bb9637246faa22db61b9a906e3a30754c
ivy997 <49655087+ivy997@users.noreply.github.com>
2021-06-09 10:51:29 +0300

    Implement email sender

--- SemesterProjectManager/SemesterProjectManager.Services/EmailSender.cs
+++ SemesterProjectManager/SemesterProjectManager.Services/EmailSender.cs
@@ -1,30 +1,53 @@
-﻿namespace WebPWrecover.Services
+﻿namespace SemesterProjectManager.Services
 {
     using Microsoft.AspNetCore.Identity.UI.Services;
-    using Microsoft.Extensions.Options;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.Options;
     using SemesterProjectManager.Services;
     using SendGrid;
     using SendGrid.Helpers.Mail;
     using System;
-    using System.Threading.Tasks;
+	using ASYNC = System.Threading.Tasks;
 
     public class EmailSender : IEmailSender
     {
+        //private readonly IConfiguration configuration;
+
+        //public EmailSender(IConfiguration configuration,
+        //    IOptions<AuthMessageSenderOptions> optionsAccessor)
+        //{
+        //    Options = optionsAccessor.Value;
+        //    this.configuration = configuration;
+        //}
+
+        //public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+
+        //public async ASYNC.Task SendEmailAsync(string email, string subject, string htmlMessage)
+        //{
+        //    var apiKey = configuration["SMPApiKey"];
+        //    var client = new SendGridClient(apiKey);
+        //    var from = new EmailAddress("avi1999@abv.bg", "Ivelina");
+        //    //var subjectTest = "Sending with SendGrid is Fun";
+        //    var to = new EmailAddress(email, "Ivan");
+        //    var plainTextContent = "Dear user,";
+        //    var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+        //    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        //    var response = await client.SendEmailAsync(msg);
+        //}
+
         public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
         {
             Options = optionsAccessor.Value;
         }
 
-        public string apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-
         public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
-        public Task SendEmailAsync(string email, string subject, string message)
+        public ASYNC.Task SendEmailAsync(string email, string subject, string message)
         {
-            return Execute(apiKey, subject, message, email);
+            return Execute(Options.SendGridKey, subject, message, email);
         }
 
-        public Task Execute(string apiKey, string subject, string message, string email)
+        public ASYNC.Task Execute(string apiKey, string subject, string message, string email)
         {
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
