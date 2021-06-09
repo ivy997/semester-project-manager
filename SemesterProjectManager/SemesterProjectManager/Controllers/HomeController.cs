2e83bed27f7c2f5c3259a665b628474fa56b2f6d
ivy997 <avi1999@abv.bg>
2021-06-09 10:49:53 +0300

    Implement email sender

--- SemesterProjectManager/SemesterProjectManager/Controllers/HomeController.cs
+++ SemesterProjectManager/SemesterProjectManager/Controllers/HomeController.cs
@@ -1,17 +1,23 @@
 ﻿namespace SemesterProjectManager.Controllers
 {
+	using Microsoft.AspNetCore.Identity.UI.Services;
 	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.Extensions.Logging;
 	using SemesterProjectManager.Models;
+	using SemesterProjectManager.Services;
 	using System.Diagnostics;
+	using ASYNC = System.Threading.Tasks;
 
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly IEmailSender emailSender;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger,
+			IEmailSender emailSender)
 		{
 			_logger = logger;
+			this.emailSender = emailSender;
 		}
 
 		public IActionResult Index()
@@ -28,6 +34,13 @@
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult SendEmail()
+		{
+			this.emailSender.SendEmailAsync("", "", "");
+
+			return this.RedirectToAction(nameof(Index));
 		}
 	}
 }
