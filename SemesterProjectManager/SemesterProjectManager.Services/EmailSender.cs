a4ea64c98b5071ca9a63e0122b05b2a7f9b388c6
ivy997 <avi1999@abv.bg>
2021-06-17 11:37:27 +0300

    Implement contact page
    - Add Contact Controller
    - Minor front-end changes

--- SemesterProjectManager/SemesterProjectManager.Services/EmailSender.cs
+++ SemesterProjectManager/SemesterProjectManager.Services/EmailSender.cs
@@ -1,16 +1,13 @@
 ﻿namespace SemesterProjectManager.Services
 {
-    using Microsoft.AspNetCore.Identity.UI.Services;
+	using Microsoft.AspNetCore.Identity.UI.Services;
 	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.Options;
-    using SemesterProjectManager.Services;
-    using SendGrid;
-    using SendGrid.Helpers.Mail;
-    using System;
+	using SendGrid;
+	using SendGrid.Helpers.Mail;
 	using System.Threading.Tasks;
-	using ASYNC = System.Threading.Tasks;
 
-    public class EmailSender : IEmailSender
+	public class EmailSender : IEmailSender
     {
         private readonly IConfiguration config;
 
@@ -23,39 +20,16 @@
 
         public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
-        public ASYNC.Task Execute(string email, string subject, string message)
-        {
-            //var client = new SendGridClient(Options.SendGridKey);
-            //var msg = new SendGridMessage()
-            //{
-            //    From = new EmailAddress("avi1999@abv.bg", Options.SendGridUser),
-            //    Subject = subject,
-            //    PlainTextContent = message,
-            //    HtmlContent = message
-            //};
-            //msg.AddTo(new EmailAddress(email));
-
-            //// Disable click tracking.
-            //// See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
-            //msg.SetClickTracking(false, false);
-
-            //return client.SendEmailAsync(msg);
-
-            return SendEmailAsync(subject, message, email);
-        }
-
-		public Task SendEmailAsync(string email, string subject, string htmlMessage)
+		public async Task SendEmailAsync(string email, string subject, string htmlMessage)
 		{
             var apiKey = config["SPM_API_KEY"];
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("avi1999@abv.bg", "Admin");
+            var from = new EmailAddress("avi1999@abv.bg", "Semester Project Manager");
             var to = new EmailAddress(email);
-            var plainTextContent = $"Hello, {email.Split('@')[0]}.";
-            //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var plainTextContent = "";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlMessage);
-
             //msg.SetClickTracking(false, false);
-            return client.SendEmailAsync(msg);
+            await client.SendEmailAsync(msg);
         }
 	}
 }
---
+++ SemesterProjectManager/SemesterProjectManager.Web.ViewModels/ContactViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SemesterProjectManager.Web.ViewModels
+{
+	public class ContactViewModel
+	{
+		public string SenderFullName { get; set; }
+
+		public string SenderEmail { get; set; }
+
+		public string StatusMessage { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+		[Display(Name = "Subject")]
+		public string MessageSubject { get; set; }
+
+		[Required]
+		[StringLength(5000, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+		[Display(Name = "Message")]
+		public string MessageContent { get; set; }
+
+		[Required]
+		[Display(Name = "Teacher")]
+		public string TeacherId { get; set; }
+
+		public IDictionary<string, string> Teachers { get; set; }
+	}
+}
